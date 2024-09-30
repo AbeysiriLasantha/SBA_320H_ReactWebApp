2e4090afd6defa84691fe8a49a0146641d7c0528
AbeysiriLasantha <lasantha.abeysisri@gmail.com>
2024-09-29 19:01:15 -0500

    Improved the styles with some CSS further

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,19 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
+      <nav className='appTitle'> 
+        <h1> MovieMap </h1> 
+        <h3> Mapping out Your Movie-Watching Journey! </h3>
+      </nav>
+
       <nav>
         <Link to="/">Search</Link>
         <Link to="/watchlist">Watchlist</Link>
       </nav>
+
     </header>
+
+    
   );
 };
 
